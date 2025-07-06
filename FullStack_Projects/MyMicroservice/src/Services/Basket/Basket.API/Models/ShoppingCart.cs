f841cfad1f87c18461f266c7ecacbf73a7764f0b
Syufan <zyf571635687@gmail.com>
2025-07-07 00:18:07 +0800

    modify the total price

--- FullStack_Projects/MyMicroservice/src/Services/Basket/Basket.API/Models/ShoppingCart.cs
+++ FullStack_Projects/MyMicroservice/src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -4,5 +4,5 @@ public class ShoppingCart
 {
     public string Username { get; set; } = string.Empty;
     public List<ShoppingCartItem> Items { get; set; } = new();
-    public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+    public decimal TotalPrice => Items.Sum(item => item.Total);
 }
--- FullStack_Projects/MyMicroservice/src/Services/Basket/Basket.API/obj/Debug/net9.0/Basket.API.AssemblyInfo.cs
+++ FullStack_Projects/MyMicroservice/src/Services/Basket/Basket.API/obj/Debug/net9.0/Basket.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43baca56f74d792761815ba1efe4da2205eb524d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basket.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
